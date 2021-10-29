6ebd138fea1e2e7840caf4400d042bfa4ad88e7a
Parjanya HK <61843804+parjanyahk@users.noreply.github.com>
2021-10-30 01:25:00 +0530

    Create javaSingletonPattern.java

---
+++ javaSingletonPattern.java
@@ -0,0 +1,17 @@
+class Singleton {
+    private volatile static Singleton instance;
+    public static String str;
+    private Singleton() {}
+    
+    static Singleton getSingleInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
